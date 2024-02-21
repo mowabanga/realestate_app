c1d682b28decf5980f6376da4cabe1e7203009d6
mowabanga <itsmundia@gmail.com>
2024-02-21 14:12:36 +0300

    Created the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes path="/" element={<Home />}></Routes>
       <Routes path="/sign-in" element={<SignIn />}></Routes>
       <Routes path="/sign-up" element={<SignUp />}></Routes>
