620818e04a18b05d785e432d5280a4115a1a5fcc
mowabanga <itsmundia@gmail.com>
2024-02-21 13:49:46 +0300

    Created the pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <h1 className="text-red-500">Hello, World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes path="/" element={<Home />}></Routes>
+      <Routes path="/sign-in" element={<SignIn />}></Routes>
+      <Routes path="/sign-up" element={<SignUp />}></Routes>
+      <Routes path="/about" element={<About />}></Routes>
+      <Routes path="/profile" element={<Profile />}></Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div className="text-red-400">Home</div>;
+}
